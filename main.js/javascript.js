20d472976f799399ff578bc2b00084bd8e6d4e02
Banaa123 <157402801+Banaa123@users.noreply.github.com>
2024-01-23 10:31:19 +0700

    Add files via upload

--- main.js/javascript.js
+++ main.js/javascript.js
@@ -168,3 +168,21 @@ window.addEventListener("mousemove", function (event) {
   }
 
 });
+
+//faqs//
+
+const faqs = document.querySelectorAll('.faq');
+
+faqs.forEach(faq => {
+  faq.addEventListener('click', () => {
+    faq.classList.toggle('open');
+
+    //change icon
+    const icon =faq.querySelector('.faq__icon i');
+    if(icon.className === 'uil uil-plus') { 
+       icon.className = "uil uil-minus"
+    } else { 
+      icon.className = 'uil uil-plus'
+    }
+  })
+})
